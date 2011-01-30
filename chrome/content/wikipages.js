6d42230b95d3b3dc4c1badef2eb7af48f5a86e73
Michael Pietsch <mp@mpietsch.com>
2011-01-30 19:05:12 +0100

    Added GPL v2 copyright notice to source files

--- chrome/content/wikipages.js
+++ chrome/content/wikipages.js
@@ -1,3 +1,20 @@
+/*
+ * This file is part of the Redmine Toolbar, a Firefox addon to manage Redmine projects more easily.
+ * Copyright (C) 2010,2011 Michael Pietsch <redminetoolbar@mpietsch.com>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */ 
 var RedmineToolbar_Wikipages = {
 
   load : function() {
